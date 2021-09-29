d598a9118c61192eed31143940ebd5f93cd83683
Andy Block <ablock91@gmail.com>
2021-09-29 00:03:12 -0400

    worked thru lots of streamlit issues and made huge performance improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 import os
 import pickle
@@ -5,121 +7,102 @@ from ImageLoader import ImageLoader, label_encoder, label_decoder
 from PIL import Image
 from PytorchAlgos import PytorchAlgos
 from KFolder import *
-import urllib.request
+# import urllib.request
 
 
-@st.cache
-def download1(url1):
-    url = url1
-    filename = url.split('/')[-1]
-    urllib.request.urlretrieve(url, filename)
-
-
-download1('https://download.pytorch.org/models/resnet101-5d3b4d8f.pth')
+# @st.cache
+# def download1(url1):
+#     url = url1
+#     filename = url.split('/')[-1]
+#     urllib.request.urlretrieve(url, filename)
+#
+#
+# download1('https://download.pytorch.org/models/resnet101-5d3b4d8f.pth')
 
 
 # Initialization
 TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 if 'SEED_VAL' not in st.session_state:
-    st.session_state['SEED_VAL'] = 42
+    st.session_state['SEED_VAL'] = 0
 
 ALB_TRX_STATES = ['hflip', 'vflip', 'color_jitter', 'rotate', 'rgb_shift']
 for trx_state in ALB_TRX_STATES:
     if trx_state not in st.session_state:
         st.session_state[trx_state] = False
 
-if 'apply_alb_trxs' not in st.session_state:
-    st.session_state['apply_alb_trxs'] = False
+
+RESIZE_FACTOR: int = 347  # each image is 1388x1040 pixels, so this value represents a 4x downsize
+MEANS: Tuple[float, float, float] = (0.485, 0.456, 0.406)
+STDS: Tuple[float, float, float] = (0.229, 0.224, 0.225)
 
 
-class ImageData:
-    transformed_image: Any
-    PIL_image: Image
-    random_cancer_type: str
-
-    def __init__(self):
-        if 'image_loader.obj' in os.listdir(os.getcwd()):
-            with open('image_loader.obj', 'rb') as f:
-                self.image_df = pickle.load(f).df
-
-        else:
-            self.image_df = ImageLoader('./Images').df
-
-        self.image_df['cancer_type'] = self.image_df['cancer_type'].map(label_encoder())
-        random.seed(st.session_state['SEED_VAL'])
-
-    def random_sample(self):
-        random_idx = random.randint(0, len(self.image_df))
-        random_image = self.image_df['img_array'].iloc[random_idx]
-        self.random_cancer_type = self.image_df['cancer_type'].iloc[random_idx]
-        return random_image, self.random_cancer_type
-
-    def capture_transformed_image(self, image):
-        self.transformed_image = image
-
-    def create_PIL_image(self, image):
-        self.PIL_image = Image.fromarray(image)
+@st.experimental_singleton
+def get_image_dataframe() -> pd.DataFrame:
+    if 'image_loader.obj' in os.listdir(os.getcwd()):
+        with open('image_loader.obj', 'rb') as f:
+            return pickle.load(f)
+    else:
+        return ImageLoader('./Images').df
 
 
-class AlbTrxs:
-
-    def __init__(self, horizontal_flip: bool, vertical_flip: bool, color_jitter: bool, rotate: bool, rgb_shift: bool):
-        self.horizontal_flip = horizontal_flip
-        self.vertical_flip = vertical_flip
-        self.color_jitter = color_jitter
-        self.rotate = rotate
-        self.rgb_shift = rgb_shift
-
-    def selected_transforms(self, image):
-        return A.Compose([A.HorizontalFlip(p=self.horizontal_flip),
-                          A.VerticalFlip(p=self.vertical_flip),
-                          A.ColorJitter(p=self.color_jitter),
-                          A.Rotate(limit=10, interpolation=cv2.BORDER_CONSTANT, p=self.rotate),
-                          A.RGBShift(p=self.rgb_shift)
-                          ])(image=image)['image']
+def get_random_sample(image_df: pd.DataFrame) -> Tuple[np.ndarray, np.int64]:
+    random.seed(st.session_state['SEED_VAL'])
+    random_idx = random.randint(0, len(image_df))
+    return image_df['img_array'].iloc[random_idx], image_df['cancer_type'].iloc[random_idx]
 
 
-class TrainedModel:
-    transformed_image: Any
-    resize_factor: int = 347  # each image is 1388x1040 pixels, so this value represents a 4x downsize
-    tl_means: Tuple[float, float, float] = (0.485, 0.456, 0.406)
-    tl_stds: Tuple[float, float, float] = (0.229, 0.224, 0.225)
+@st.experimental_memo
+def apply_alb_trxs(image_: np.ndarray, horizontal_flip: bool, vertical_flip: bool, color_jitter: bool,
+                   rotate: bool, rgb_shift: bool) -> np.ndarray:
 
-    def __init__(self, image_data: ImageData):
-        self.image_data = image_data
+    return A.Compose([A.HorizontalFlip(p=horizontal_flip),
+                      A.VerticalFlip(p=vertical_flip),
+                      A.ColorJitter(p=color_jitter),
+                      A.Rotate(limit=10, interpolation=cv2.BORDER_CONSTANT, p=rotate),
+                      A.RGBShift(p=rgb_shift)
+                      ])(image=image_)['image']
 
-        pytorch_algos = PytorchAlgos(resnet18=False, resnet34=False, vgg13=False, vgg16=False, vgg13_bn=False,
-                                     vgg16_bn=False, mobilenet_v2=False)
-        self.model = pytorch_algos.RESNET101.model
-        self.model.load_state_dict(torch.load('./best_model.pth', map_location=TORCH_DEVICE))
 
-    def default_transforms(self):
-        self.transformed_image = A.Compose([A.Normalize(mean=self.tl_means, std=self.tl_stds),
-                                            A.LongestMaxSize(self.resize_factor),
-                                            ToTensorV2()
-                                            ])(image=self.image_data.transformed_image)['image']
+@st.experimental_singleton
+def create_model(torch_device: torch.device):
+    pytorch_algos = PytorchAlgos(resnet101=True, resnet18=False, resnet34=False, vgg13=False, vgg16=False,
+                                 vgg13_bn=False, vgg16_bn=False, mobilenet_v2=False)
+    model = pytorch_algos.RESNET101.model
+    model.load_state_dict(torch.load('./best_model.pth', map_location=torch_device))
+    return model
 
-    def make_prediction(self):
-        data = {'cancer_type': [self.image_data.random_cancer_type], 'img_array': [self.transformed_image]}
-        input_df = pd.DataFrame(data=data)
 
-        dataset = PytorchImagesDataset(input_df)
-        dataloader = DataLoader(dataset, batch_size=1, num_workers=0)
+@st.experimental_memo
+def pytoch_default_transforms(image_: np.ndarray, means: Tuple[float, float, float],
+                              stds: Tuple[float, float, float], resize_factor: int) -> np.ndarray:
+    return A.Compose([A.Normalize(mean=means, std=stds),
+                      A.LongestMaxSize(resize_factor),
+                      ToTensorV2()
+                      ])(image=image_)['image']
 
-        for inputs, labels in dataloader:
-            inputs, labels = inputs.to(TORCH_DEVICE), labels.to(TORCH_DEVICE)
 
-            with torch.set_grad_enabled(False):
-                outputs = self.model(inputs)
-                _, preds = torch.max(outputs, 1)
+@st.experimental_memo
+def make_prediction(_model_, _image_: np.ndarray, cancer_type: int, torch_device: torch.device):
+    data = {'cancer_type': [cancer_type], 'img_array': [_image_]}
+    input_df = pd.DataFrame(data=data)
 
-            decoder = label_decoder()
-            prediction = int(preds)
-            prediction = decoder[prediction]
-            self.image_data.random_cancer_type = decoder[self.image_data.random_cancer_type]
+    dataset = PytorchImagesDataset(input_df)
+    dataloader = DataLoader(dataset, batch_size=1, num_workers=0)
 
-        return self.image_data.random_cancer_type, prediction
+    for inputs, labels in dataloader:
+        inputs, labels = inputs.to(torch_device), labels.to(torch_device)
+
+        with torch.set_grad_enabled(False):
+            outputs = _model_(inputs)
+            _, preds = torch.max(outputs, 1)
+
+        decoder = label_decoder()
+        prediction = int(preds)
+        prediction = decoder[prediction]
+        cancer_type = decoder[cancer_type]
+
+    return cancer_type, prediction
 
 
 header = st.container()
@@ -137,6 +120,9 @@ with header:
     """)
     st.write('')
 
+
+IMAGE: np.ndarray
+CANCER_TYPE: np.int64
 
 with image:
 
@@ -164,18 +150,15 @@ with image:
         st.session_state['SEED_VAL'] += 1
 
     # albumentations transformations and image display
-    alb_trxs = AlbTrxs(*session_states)
-    image_data = ImageData()
-    image, cancer_type = image_data.random_sample()
-    image = alb_trxs.selected_transforms(image)
-    image_data.capture_transformed_image(image)
-    image_data.create_PIL_image(image)
-    image_col.image(image_data.PIL_image, use_column_width=True)
+    image_data = get_image_dataframe()
+    IMAGE, CANCER_TYPE = get_random_sample(image_data)
+    IMAGE = apply_alb_trxs(IMAGE, *session_states)
+    image_col.image(Image.fromarray(IMAGE), use_column_width=True)
 
 
 with run_model:
-    trained_model = TrainedModel(image_data)
-    trained_model.default_transforms()
+    model = create_model(TORCH_DEVICE)
+    IMAGE = pytoch_default_transforms(IMAGE, MEANS, STDS, RESIZE_FACTOR)
 
     run_model_col, prediction_col, actual_col = st.columns(3)
     prediction_col.subheader('Predicted Type:')
@@ -184,7 +167,7 @@ with run_model:
     run_model_col.subheader('')
     run_model_button = run_model_col.button('Run Model')
     if run_model_button:
-        actual, prediction = trained_model.make_prediction()
+        actual, prediction = make_prediction(model, IMAGE, CANCER_TYPE, TORCH_DEVICE)
         prediction_col.write(prediction)
         actual_col.write(actual)
     else:
